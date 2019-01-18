912b9eb5e8df7a290fb8b6e3fece28da24e3b280
fariscode <farizjilani@gmail.com>
2019-01-18 19:48:59 +0500

    all

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+
+export default class dishDetailComponent extends Component {
+
+  renderComments = (renderComments) => {
+    const commentsList = renderComments.map((comment) =>
+      <div key={comment.id}>
+        <p>{comment.comment}</p>
+        <p>{`-- ${comment.author}, ${comment.date}`}</p>
+      </div>
+    );
+    return (
+      <>
+        <h4>Comments</h4>
+        {commentsList}
+      </>
+    )
+  }
+
+  renderDish = (dish) => {
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  }
+
+  render() {
+    const { dish } = this.props;
+    const card = dish ? (
+      <div className='row'>
+        <div className="col-12 col-md-5 m-1">
+          {this.renderDish(dish)}
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          {this.renderComments(dish.comments)}
+        </div>
+      </div>
+    )
+      : <div></div>;
+
+    return card;
+  }
+}
